cd5a9b55ee0793d403b62a909943bff2a92e6eed
liuzexin <liuzexin@sharetimes.cn>
2017-01-20 17:25:42 +0800

    modified:   README.md 	modified:   jquery-plugin.marquee-lottery.js

---
+++ README.md
@@ -0,0 +1,73 @@
+# Marquee Lottery #
+
+## Instruction ##
+
+Marquee Lottery jQuery plugin for more convenient create lottery activity.You only need to care about the `CSS` or `HTML` code.
+
+## Installation ##
+**Composer**
+```
+composer require bower-asset/marquee-lottery
+```
+**Bower**
+```
+bower install marquee-lottery
+```
+**NPM**
+```
+npm install marquee-lottery
+```
+
+## Example ##
+
+```javascript
+$(function(){
+   $('.btn').marquee({
+       activeClass:'active-img',
+       finishCallback:function(){
+           alert(1);
+       },
+       ele:[
+           '.first-img',
+           '.second-img',
+           '.third-img',
+           '.4th-img',
+           '.5th-img',
+           '.6th-img',
+           '.7th-img',
+           '.8th-img'
+       ],
+       desIndex:3
+   });
+    $('.btn').click(function(){
+        $('.btn').marquee('start');
+    });
+});
+```
+![marquee-lottery.gif](https://github.com/liuzexin/Image/blob/master/marquee-lottery.gif?raw=true)
+
+Before using the `marquee-lottery`, you should layout the page first.
+
+## Usage ##
+
+First of all,you should know that speed mode only support  one, which `ease-in-ease-out` mode.
+
+### Configuration ###
+
+`ele`:The jQuery selector of gift image，its order according to you need order.If you want let second image at the begin of running, you should put it in first index of ele.
+`maxTime`:The max time of the gift image switch.
+`minTime`:The min time of the gift image switch.
+`desIndex`:This param should resolve from server, the gift image stop at `desIndex` position.
+`roundTotal`:Number of circles for running.In fact, circles is `roundTotal + 1`.The last of circle will select the `desIndex`.
+`finishCallback`:This callback is called at the end of running.
+`delay`:Delay the given time to call the `finishCallback`. 
+`activeClass`:When pass the every gift image add the `activeClass` class. 
+`reduceTimeStep`:The time reduce in the speed up.
+`addTimeStep`:The time add in the slow down.
+
+`$('#id').marquee('start');` Start marquee right now.
+
+
+[CoderLiu](http://blog.liuzexin.top)
+
+
--- jquery-plugin.marquee-lottery.js
+++ jquery-plugin.marquee-lottery.js
@@ -6,13 +6,13 @@
     var methods = {
         init : function( options ) {
             var $this = $(this);
-            var  roundTotal= 4,totalTime=1500,minTime=100,reduceStepTime=200,addTimeStep=50,desIndex= 0;
+            var  roundTotal= 4,maxTime=1500,minTime=100,reduceStepTime=200,addTimeStep=50,desIndex= 0;
 
             var defaultSettings = {
                 ele:null,
                 desIndex:desIndex,
                 roundTotal:roundTotal,
-                totalTime:totalTime,
+                maxTime:maxTime,
                 minTime:minTime,
                 addTimeStep:addTimeStep,
                 reduceTimeStep:reduceStepTime,
@@ -26,14 +26,14 @@
             if (settings.desIndex > settings.ele.length -1){
                 $.error('Marquee \'desIndex \' error : more than \'ele\' length.');
                 return $this;
-            }else if(settings.minTime > settings.totalTime || settings.minTime == 0){
-                $.error('Marquee \'minTime\' error : more than \'totalTime\' or equal 0.');
+            }else if(settings.minTime > settings.maxTime || settings.minTime == 0){
+                $.error('Marquee \'minTime\' error : more than \'maxTime\' or equal 0.');
                 return $this;
-            }else if(settings.addTimeStep <= 0 || settings.addTimeStep > settings.totalTime){
-                $.error('Marquee \'addTimeStep\' error : less than 0 or equal 0 more than \`totalTime\`.');
+            }else if(settings.addTimeStep <= 0 || settings.addTimeStep > settings.maxTime){
+                $.error('Marquee \'addTimeStep\' error : less than 0 or equal 0 more than \`maxTime\`.');
                 return $this;
-            }else if(settings.reduceTimeStep <= 0 || settings.reduceTimeStep > settings.totalTime){
-                $.error('Marquee \'reduceTimeStep\' error : less than 0 or equal 0 or more than \`totalTime\`.');
+            }else if(settings.reduceTimeStep <= 0 || settings.reduceTimeStep > settings.maxTime){
+                $.error('Marquee \'reduceTimeStep\' error : less than 0 or equal 0 or more than \`maxTime\`.');
                 return $this;
             }else if(settings.roundTotal <= 0){
                 $.error('Marquee \'roundTotal\' error : less than 0 or equal 0.');
@@ -51,7 +51,7 @@
             var currentRound=0,currentIndex= 1, sArr= settings.ele, sArrCount = settings.ele.length, timer=null, desIndex=settings.desIndex,
                 roundTotal=settings.roundTotal,delayTime=settings.delayTime,
                 minTime=settings.minTime,reduceStepTime=settings.reduceTimeStep,addTimeStep=settings.addTimeStep,
-                totalTime=settings.totalTime,currentTime=totalTime;
+                maxTime=settings.maxTime,currentTime=maxTime;
             //Fix the bug of the secondary of start, the `desIndex` is the active class.
             $(sArr[desIndex]).removeClass(settings.activeClass);
             //At the begin of start, should add class for the first element of `sArr`.
@@ -79,7 +79,7 @@
                 }
 
                 if (currentRound > roundTotal - 1) {
-                    currentTime = currentTime + addTimeStep > totalTime ? totalTime : currentTime + addTimeStep;
+                    currentTime = currentTime + addTimeStep > maxTime ? maxTime : currentTime + addTimeStep;
                 } else {
                     currentTime = currentTime - reduceStepTime < minTime ? minTime : currentTime - reduceStepTime;
                 }
